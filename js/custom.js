1f047b9b2481680afae4260be1c29a708b320924
Pester, Samuel B. (S&amp;T-Student) <sbphx5@mst.edu>
2016-05-04 15:28:06 -0500

    Reordered gifs

--- js/custom.js
+++ js/custom.js
@@ -16,9 +16,7 @@ var image_landing = ['../images/landing/landing1.gif',
                     '../images/landing/landing16.gif', 
                     '../images/landing/landing17.gif', 
                     '../images/landing/landing18.gif', 
-                    '../images/landing/landing19.gif',
-                    '../images/landing/landing20.gif',
-                    '../images/landing/landing21.gif'];
+                    '../images/landing/landing19.gif'];
 
 var image_nav = ['../images/nav/nav1.gif', 
                 '../images/nav/nav2.gif', 
