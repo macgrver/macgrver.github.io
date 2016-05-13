9b3090d9e36dcde2434724bec25815f8743b0d21
Pester, Samuel B. (S&amp;T-Student) <sbphx5@mst.edu>
2016-05-13 16:51:23 -0500

    Added new icons for nav-menu

--- js/profilecard.js
+++ js/profilecard.js
@@ -1,59 +1,58 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-var quote_array = ["I loves gifs... way too much.", 
-                   "Let's design a world with better internet.", 
+var quote_array = ["I loves gifs... way too much.",
                   "Time is an illusion. Lunchtime doubly so.",
-                  "On the internet nobody knows you're a dog."];
+                  "On the internet nobody knows you're a dog.",
+                  "Graphic designer with a taste for ambitious web experiences."];
 var hasClicked = false;
 
 $(".profile_card").css({
-  backgroundColor: '#' + randomColor
+    backgroundColor: '#' + randomColor
 });
 
 $(".title > h1, .title > h2").css({
-  color: '#' + randomColor
+    color: '#' + randomColor
 });
 
-$('.social > .fa, .social > .fa-user, .profile_image > .close').on('click', function () {    
-    if(hasClicked === false){
-    $(".profile_card").css({
+$('.social, .profile_image > .close').on('click', function () {
+    if (hasClicked === false) {
+        $(".profile_card").css({
 
-        backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16)
+            backgroundColor: '#' + Math.floor(Math.random() * 16777215).toString(16)
 
-    });
-    var rando_quote = quote_array[Math.round(Math.random() * quote_array.length-1)];
-    $('.sub_text > p').text(rando_quote);
-    $('.deck').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-        function () {
-            $(this).removeClass('animated bounceIn');
         });
-    $('.wrapper').toggleClass('hidden');
-    $('.wrapper_deck').toggleClass('hidden');
-    $('.landing > img, .landing > p').toggleClass('hidden');
-    hasClicked = true;
-}
-    else if(hasClicked === true){
+        var rando_quote = quote_array[Math.round(Math.random() * quote_array.length)];
+        $('.sub_text > p').text(rando_quote);
+        $('.deck').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+            function () {
+                $(this).removeClass('animated bounceIn');
+            });
+        $('.wrapper').toggleClass('hidden');
+        $('.wrapper_deck').toggleClass('hidden');
+        $('.landing > img, .landing > p').toggleClass('hidden');
+        hasClicked = true;
+    } else if (hasClicked === true) {
         $('.deck').addClass('animated bounceOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
-        function () {
-            $(this).removeClass('animated bounceOut');
-            $('.wrapper').toggleClass('hidden');
-            $('.wrapper_deck').toggleClass('hidden');
-            $('.landing > img, .landing > p').toggleClass('hidden');
-        });
-    
+            function () {
+                $(this).removeClass('animated bounceOut');
+                $('.wrapper').toggleClass('hidden');
+                $('.wrapper_deck').toggleClass('hidden');
+                $('.landing > img, .landing > p').toggleClass('hidden');
+            });
+
         hasClicked = false;
     }
 });
 
 $('.social > .fa, .social > .fa-user').hover(
-  function() {
-    $(this).css({
-      color: '#' + Math.floor(Math.random() * 16777215).toString(16)
-    });
-  },
-  function() {
-    $(this).css({
-      color: '#fff'
-    });
-  }
+    function () {
+        $(this).css({
+            color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+        });
+    },
+    function () {
+        $(this).css({
+            color: '#fff'
+        });
+    }
 );
