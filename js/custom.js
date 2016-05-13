0d8efee55cae4650e0d6ee8cb63d733c3a5f05d8
Pester, Samuel B. (S&amp;T-Student) <sbphx5@mst.edu>
2016-05-13 09:19:44 -0500

    Added new gifs for .profile_image

--- js/custom.js
+++ js/custom.js
@@ -27,10 +27,21 @@ var image_nav = ['../images/nav/nav1.gif',
                 '../images/nav/nav7.gif', 
                 '../images/nav/nav8.gif', ];
 
+var image_profile = ['../images/profile_image/profile_image1.gif',
+                    '../images/profile_image/profile_image2.gif',
+                    '../images/profile_image/profile_image3.gif',
+                    '../images/profile_image/profile_image4.gif',
+                    '../images/profile_image/profile_image5.gif',
+                    '../images/profile_image/profile_image6.gif',
+                    '../images/profile_image/profile_image7.gif',
+                    '../images/profile_image/profile_image8.gif',
+                    '../images/profile_image/profile_image9.gif'
+                    ];
+
 $('.landing, .wrapper_deck').css({'background-image': 'url(' + image_landing[Math.floor(Math.random() * image_landing.length)] + ')'});
 $('.menu-nav').css({'background-image': 'url(' + image_nav[Math.floor(Math.random() * image_nav.length)] + ')'});
 $('.footie').css({'background-image': 'url(' + image_nav[Math.floor(Math.random() * image_nav.length)] + ')'});
-$('.profile_image_background').css({'background-image': 'url(' + image_landing[Math.floor(Math.random() * image_landing.length)] + ')'});
+$('.profile_image_background').css({'background-image': 'url(' + image_profile[Math.floor(Math.random() * image_profile.length)] + ')'});
 
 $(".develop").click(function() {
     $('html, body').animate({
