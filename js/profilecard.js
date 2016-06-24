28cae786c2a26d6e67501459dad7b2ff9b0e15fe
Pester, Samuel B. (S&amp;T-Student) <sbphx5@mst.edu>
2016-06-24 10:19:06 -0500

    Merged again

--- js/profilecard.js
+++ js/profilecard.js
@@ -1,30 +1,12 @@
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-var quote_array = ["I loves gifs... way too much."
-
-
-    
-    , "Time is an illusion. Lunchtime doubly so."
-
-
-    
-    , "On the internet nobody knows you're a dog."
-
-
-    
-    , "Graphic designer with a taste for ambitious web experiences."
-
-
-    
-    , "Is this too many gifs? Hmmm, I'm not sure if there's enough to be honest."
-
-
-    
-    , "Sometimes science is a lot more art, than science. A lot of people don't get that."];
-var hasClicked = false;
-var devhasClicked = false;
-var illhasClicked = false;
-var designhasClicked = false;
+var quote_array = ["I loves gifs... way too much.",
+                  "Time is an illusion. Lunchtime doubly so.",
+                  "On the internet nobody knows you're a dog.",
+                  "Graphic designer with a taste for ambitious web experiences.",
+                  "Is this too many gifs? Hmmm, I'm not sure if there's enough to be honest.",
+                  "Sometimes science is a lot more art, than science. A lot of people don't get that."];
+var hasClicked = true;
 
 $(".profile_card").css({
     backgroundColor: '#' + randomColor
@@ -47,23 +29,16 @@ $('.social, .deck > .close').on('click', function () {
             function () {
                 $(this).removeClass('animated bounceIn');
             });
-        $('.center').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
-            , function () {
+        $('.profile_image').addClass('animated bounceIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+            function () {
                 $(this).removeClass('animated bounceIn');
             });
         $('.wrapper').toggleClass('hidden');
         $('.wrapper_deck').toggleClass('hidden');
         $('.landing > img, .landing > p').toggleClass('hidden');
-        hasClicked = true;
-    } else if (hasClicked === true) {
-        $('.deck').addClass('animated bounceOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
-            , function () {
-                $(this).removeClass('animated bounceOut');
-                $('.wrapper').toggleClass('hidden');
-                $('.wrapper_deck').toggleClass('hidden');
-                $('.landing > img, .landing > p').toggleClass('hidden');
-            });
-        $('.profile_image').addClass('animated bounceOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+        hasClicked = false;
+    } else if (hasClicked === false) {
+        $('.deck').addClass('animated bounceOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
             function () {
                 $(this).removeClass('animated bounceOut');
                 $('.wrapper').toggleClass('hidden');
